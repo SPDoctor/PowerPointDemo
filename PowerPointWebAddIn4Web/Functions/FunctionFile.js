c0688154f25f112801c977dc7130d8a109ec43a7
Bill Ayers <BillA@flosim.com>
2018-09-15 09:22:15 +0100

    Added files

---
+++ PowerPointWebAddIn4Web/Functions/FunctionFile.js
@@ -0,0 +1,11 @@
+﻿// The initialize function must be run each time a new page is loaded.
+(function () {
+    Office.initialize = function (reason) {
+        // If you need to initialize something you can do so here.
+    };
+})();
+
+function doSomething(event) {
+    Office.context.document.setSelectedDataAsync("Did something!");
+    event.completed();
+}
